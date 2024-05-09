5c6661f4500c70fda6590707fbeaedf721a17ae7
Nancy Ramírez <51838679+Nancy-Ramirez@users.noreply.github.com>
2024-05-08 19:56:18 -0600

    bases ts

---
+++ src/app/Gestiones/Empleados/gestion-empleados.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gestion-empleados',
+  standalone: true,
+  imports: [],
+  templateUrl: './gestion-empleados.component.html',
+  styleUrl: './gestion-empleados.component.css'
+})
+export class GestionEmpleadosComponent {
+
+}
